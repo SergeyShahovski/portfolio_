b15fe90970bb58fb5af427b9cc8e94d3683fb04b
SergeyShahovski <127224179+SergeyShahovski@users.noreply.github.com>
2023-03-13 19:35:58 +0300

    Add files via upload

---
+++ script/form.js
@@ -0,0 +1,10 @@
+document.getElementById('form_btn').onclick = function() {
+document.getElementById('form').classList.add('hidden_form');
+
+}
+
+
+
+document.getElementById('btn').onclick = function() {
+document.getElementById('form').classList.toggle('hidden_form');
+}
